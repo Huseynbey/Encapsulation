e621c4a9ac60206df4fcd60cb27d072b82bc76f7
Huseyn Baymammadli <huseynnb@code.edu.az>
2023-03-05 17:55:39 +0400

    Add project files.

---
+++ encapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class cls = new Class("678904", "Alex", "Foxtrot");
+            cls.Article = "USB-hab";
+            cls.Quantity = "6";
+            cls.Price = 30;
+            cls.CostCalculation(false);
+        }
+    }
+}
